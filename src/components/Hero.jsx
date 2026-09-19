4a084545051eaa3fe702cfaa755211088279973c
Megha <am.megha.gr@gmail.com>
2026-09-19 22:38:29 +0530

    Service Session

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import assets from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div id='hero' className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 
+    <div id='home' className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 
     lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700
     dark:text-white'>
 
